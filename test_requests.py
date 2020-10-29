7a1bdff198b1c1138a83a79510d72d6d082c02a6
sitdikov_denis <fomk@mail.ru>
2020-10-29 23:13:10 +0300

    first_commit

--- test_requests.py
+++ test_requests.py
@@ -1,6 +1,18 @@
-import vk
+import requests
 
-session = vk.Session(access_token='da1a7ce8efa4236d8d8dbf53997bedbca897fcd211266087c25bf1650b717142b500f382f6c55d813a52b')
-vk_api = vk.API(session)
-loadi = str(vk_api.users.get(user_id=1))
-print(loadi)
+params = {
+            'user_id': 59695190,
+            'fields': 'bdate',
+            'v': '5.52',
+            'access_token': 'da1a7ce8efa4236d8d8dbf53997bedbca897fcd211266087c25bf1650b717142b500f382f6c55d813a52b'}
+
+
+params = {
+    "screen_name": "rambler",
+    'v': '5.52',
+    'access_token': 'da1a7ce8efa4236d8d8dbf53997bedbca897fcd211266087c25bf1650b717142b500f382f6c55d813a52b'
+}
+
+r = requests.get('https://api.vk.com/method/utils.resolveScreenName', params=params)
+
+print(r.content)
